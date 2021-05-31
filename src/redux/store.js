b486d1a24e4cfd76e8fae6296f09751fa02f9544
Igor <70631855+Igor-Tomilo@users.noreply.github.com>
2021-05-31 17:11:52 +0300

    /

---
+++ src/redux/store.js
@@ -0,0 +1,47 @@
+/* eslint-disable import/no-anonymous-default-export */
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+import {
+  configureStore,
+  getDefaultMiddleware,
+  combineReducers,
+} from '@reduxjs/toolkit';
+import storage from 'redux-persist/lib/storage';
+
+import contactsReducer from './contacts/contacts-reducer';
+
+const contactsPersistConfig = {
+  key: 'contacts',
+  storage,
+  blacklist: ['filter'],
+};
+
+const middleware = [
+  ...getDefaultMiddleware({
+    serializableCheck: {
+      ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+    },
+  }),
+];
+
+const rootReducer = combineReducers({
+  contacts: persistReducer(contactsPersistConfig, contactsReducer),
+});
+
+const store = configureStore({
+  reducer: rootReducer,
+  devTools: process.env.NODE_ENV === 'development',
+  middleware,
+});
+
+const persistor = persistStore(store);
+
+export default { store, persistor };
